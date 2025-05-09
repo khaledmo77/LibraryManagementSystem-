915401d081e981988bc534e61b2cf8d33272e91d
khaledmo77 <khaledmo77@outlook.com>
2025-05-10 00:08:45 +0300

    adding partialview to display book details and borrowing status

---
+++ LiberaryManagmentSystem/Views/Book/_BookDetailsPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LiberaryManagmentSystem.Views.Book
+{
+    public class _BookDetailsPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ LiberaryManagmentSystem/Views/Book/_BorrowingStatusPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LiberaryManagmentSystem.Views.Book
+{
+    public class _BorrowingStatusPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
